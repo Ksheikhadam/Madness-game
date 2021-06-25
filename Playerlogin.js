0186f5a2305bc91b171d25d5fb07cee6a17f54ce
Ksheikhadam <72895367+Ksheikhadam@users.noreply.github.com>
2021-06-25 15:40:27 -0500

    Create Playerlogin.js

---
+++ Playerlogin.js
@@ -0,0 +1,7 @@
+const user = new User();
+const userManager = user.Manager();
+Text.User.Name = User.name(/*Username*/);
+Text.User.Password = user.Password();
+else{
+Text.User.Password = ('hudfguier11');
+};
